4a88bb28ee1e2cb0d1d639e7c763cf9cae8ed05d
Mikel3010 <mikelgremi30@gmail.com>
2023-03-02 21:14:41 +0100

    es senza bonus

---
+++ js/main.js
@@ -0,0 +1,30 @@
+'use strict';
+// funzioni 
+
+function myCreateElement(htmlElement, className,htmlValeu){
+    const element = document.createElement(htmlElement);
+    element.classList.add(className);
+    element.innerText = htmlValeu;
+
+    element.addEventListener('click', function(){
+        element.classList.add('cell-onclick');
+        console.log(htmlValeu)
+    });
+    return element;
+}
+
+function myAppendElement(containerElement, htmlElement){
+    containerElement.append(htmlElement)
+}
+// main 
+let cellNumber = 100; 
+const containerBoard = document.querySelector('.board')
+const buttonPlay = document.getElementById('play');
+
+buttonPlay.addEventListener('click', function(){ 
+    containerBoard.innerHTML='';
+    for(let i = 1; i <= cellNumber ; i++ ){
+        const createdElement = myCreateElement('div', 'cell', i);
+        myAppendElement(containerBoard, createdElement);  
+    } 
+})
